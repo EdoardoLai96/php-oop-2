38a7f5cbaa0706e9ce447886e407b212c586a994
Edoardo Lai <edoardolai96@gmail.com>
2022-03-16 18:14:06 +0100

    fix parametro "payment"

--- classes/User.php
+++ classes/User.php
@@ -13,11 +13,11 @@
     public function purchase($payment = "failed"){
         if($this->credit_card_state == "valid" && $this->registered == true){
             $this->discount = 20;
-            $payment == "success";
+            $payment == "successed";
             return "Ordine eseguito con successo, sconto applicato";
         }else if($this->credit_card_state == "valid" && $this->registered == false){
             $this->discount = 0;
-            $payment == "success";
+            $payment == "successed";
             return "Ordine eseguito con successo";
         }else if($this->credit_card_state == "expired"){
             $payment == "failed";
